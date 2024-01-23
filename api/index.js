8ce1b1aca86d7848caf2433d6e2dc5241ba94634
Sanjay-Nathwani <sanjaynathwani1610@gmail.com>
2024-01-24 01:05:11 +0530

    created test api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 
+// routes import
+import userRoutes from "./routes/user.route.js";
+
 dotenv.config();
 
 mongoose
@@ -14,3 +17,11 @@ const app = express();
 app.listen(7000,() => {
     console.log('Server is running on port 7000');
 });
+
+// test api
+app.get("/test",(req,res)=>{
+    res.json({message : "API is working!"});
+})
+
+// user routes
+app.use("/api/user",userRoutes);
