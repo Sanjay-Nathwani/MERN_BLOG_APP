5f2e98a1d306f382750c0d79d56aab9f80af7282
Sanjay-Nathwani <sanjaynathwani1610@gmail.com>
2024-01-30 01:35:51 +0530

    verifyToken name changed to verifyUser

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { verifyToken } from "../utils/verifyUser.js";
+import { verifyUser } from "../utils/verifyUser.js";
 import {
   create,
   deletepost,
@@ -9,9 +9,9 @@ import {
 
 const router = express.Router();
 
-router.post("/create", verifyToken, create);
+router.post("/create", verifyUser, create);
 router.get("/getposts", getposts);
-router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
-router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
+router.delete("/deletepost/:postId/:userId", verifyUser, deletepost);
+router.put("/updatepost/:postId/:userId", verifyUser, updatepost);
 
 export default router;
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -7,15 +7,15 @@ import {
   test,
   updateUser,
 } from "../controllers/user.controller.js";
-import { verifyToken } from "../utils/verifyUser.js";
+import { verifyUser } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.get("/test", test);
-router.put("/update/:userId", verifyToken, updateUser);
-router.delete("/delete/:userId", verifyToken, deleteUser);
-router.post("/signout", signout);
-router.get("/getusers", verifyToken, getUsers);
-router.get("/:userId", getUser);
+router.put("/update/:userId",verifyUser, updateUser);
+router.delete("/delete/:userId", verifyUser, deleteUser);
+router.post("/signout",signout);
+router.get("/getusers",verifyUser, getUsers);
+router.get("/:userId",getUser);
 
 export default router;
