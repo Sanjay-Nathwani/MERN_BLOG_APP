11ddae863cc82beea4eaa5b89a5e0e93a6afd370
Sanjay-Nathwani <sanjaynathwani1610@gmail.com>
2024-01-30 01:22:50 +0530

    final commit

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,14 +1,17 @@
 import express from "express";
-import { createComment, deleteComment, editComment, getPostComments, getcomments, likeComment } from "../controllers/comment.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
+import {
+  create,
+  deletepost,
+  getposts,
+  updatepost,
+} from "../controllers/post.controller.js";
 
 const router = express.Router();
 
-router.post("/create",verifyToken,createComment);
-router.get("/getPostComments/:postId",getPostComments);
-router.put("/likeComment/:commentId",verifyToken,likeComment);
-router.put("/editComment/:commentId",verifyToken,editComment);
-router.delete("/deleteComment/:commentId", verifyToken, deleteComment);
-router.get("/getcomments",verifyToken,getcomments);
+router.post("/create", verifyToken, create);
+router.get("/getposts", getposts);
+router.delete("/deletepost/:postId/:userId", verifyToken, deletepost);
+router.put("/updatepost/:postId/:userId", verifyToken, updatepost);
 
 export default router;
