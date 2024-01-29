2b4ece5f10a47ba9dcae1c4647a6b79deb18d7fd
Sanjay-Nathwani <sanjaynathwani1610@gmail.com>
2024-01-29 18:19:05 +0530

    added dashboard overview to the admin dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import DashProfile from "../components/DashProfile";
 import DashPosts from "../components/DashPosts";
 import DashUsers from "../components/DashUsers";
 import DashComments from "../components/DashComments";
+import DashboardComp from "../components/DashboardComp";
 
 export default function Dashboard() {
 
@@ -38,6 +39,9 @@ export default function Dashboard() {
 
       {/* comments */}
       {tab === "comments" && <DashComments />}
+
+      {/* dasboard comp */}
+      {tab === "dash" && <DashboardComp />}
     </div>
   )
 }
